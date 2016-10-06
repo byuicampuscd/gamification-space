c65952b385e7d733de1ef8f54cb32b2c79ca3bab
Merrill <gshawm@byui.edu>
2016-10-06 15:03:49 -0600

    Added gitignore. Moved constant to settings file and began teh stubbing out process!

---
+++ settings.js
@@ -0,0 +1,42 @@
+/* beautify preserve:start */
+var constant = {
+    BARS_LEFT: 427,
+    BARS_RIGHT: 877,
+    HEALTH_RIGHT: 587,
+    RANK_POINTS_LEFT: 717,
+    MISSING_WIDTH: 0,
+    RANKS: [
+        {name: "Citizen",                           file: "RankingIcons/1.png"},
+        {name: "Recruit",                           file: "RankingIcons/2.png"},
+        {name: "4th Class",                         file: "RankingIcons/3.png"},
+        {name: "3rd Class",                         file: "RankingIcons/4.png"},
+        {name: "2nd Class",                         file: "RankingIcons/5.png"},
+        {name: "1st Class",                         file: "RankingIcons/6.png"},
+        {name: "3rd Class Spaceship",               file: "RankingIcons/7.png"},
+        {name: "2nd Class Spaceship",               file: "RankingIcons/8.png"},
+        {name: "1st Class Spaceship",               file: "RankingIcons/9.png"},
+        {name: "3rd Class Hexagon",                 file: "RankingIcons/10.png"},
+        {name: "2nd Class Hexagon",                 file: "RankingIcons/11.png"},
+        {name: "1st Class Hexagon",                 file: "RankingIcons/12.png"},
+        {name: "Chief",                             file: "RankingIcons/13.png"},
+        {name: "Senior Chief",                      file: "RankingIcons/14.png"},
+        {name: "Master Chief",                      file: "RankingIcons/15.png"},
+        {name: "3rd Ensign",                        file: "RankingIcons/16.png"},
+        {name: "2nd Ensign",                        file: "RankingIcons/17.png"},
+        {name: "1st Ensign",                        file: "RankingIcons/18.png"},
+        {name: "Lieutenant Junior Grade 2nd Class", file: "RankingIcons/19.png"},
+        {name: "Lieutenant Junior Grade 1st Class", file: "RankingIcons/20.png"},
+        {name: "3rd Class Lieutenant",              file: "RankingIcons/21.png"},
+        {name: "2nd Class Lieutenant",              file: "RankingIcons/22.png"},
+        {name: "1st Class Lieutenant",              file: "RankingIcons/23.png"},
+        {name: "Lieutenant Commander",              file: "RankingIcons/24.png"},
+        {name: "Commander",                         file: "RankingIcons/25.png"},
+        {name: "Captain",                           file: "RankingIcons/26.png"},
+        {name: "Rear Admiral",                      file: "RankingIcons/27.png"},
+        {name: "Vice Admiral",                      file: "RankingIcons/28.png"},
+        {name: "Admiral",                           file: "RankingIcons/29.png"},
+        {name: "Delegate",                          file: "RankingIcons/30.png"},
+        {name: "President",                         file: "RankingIcons/31.png"}
+    ]
+};
+/* beautify preserve:end */
