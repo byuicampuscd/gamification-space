7243f8aff7e559cbf1734c6eaf4529c4148a1061
Grant Merrill <grant.merrill@live.com>
2016-10-14 14:55:56 -0600

    Changed the rank names.

--- settings.js
+++ settings.js
@@ -8,37 +8,37 @@ var constant = {
     RANK_POINTS_LEFT: 717,
     MISSING_WIDTH: 0,
     RANKS: [
-        {name: "Citizen",                           file: "RankingIcons/1.png"},
-        {name: "Recruit",                           file: "RankingIcons/2.png"},
-        {name: "4th Class",                         file: "RankingIcons/3.png"},
-        {name: "3rd Class",                         file: "RankingIcons/4.png"},
-        {name: "2nd Class",                         file: "RankingIcons/5.png"},
-        {name: "1st Class",                         file: "RankingIcons/6.png"},
-        {name: "3rd Class Spaceship",               file: "RankingIcons/7.png"},
-        {name: "2nd Class Spaceship",               file: "RankingIcons/8.png"},
-        {name: "1st Class Spaceship",               file: "RankingIcons/9.png"},
-        {name: "3rd Class Hexagon",                 file: "RankingIcons/10.png"},
-        {name: "2nd Class Hexagon",                 file: "RankingIcons/11.png"},
-        {name: "1st Class Hexagon",                 file: "RankingIcons/12.png"},
-        {name: "Chief",                             file: "RankingIcons/13.png"},
-        {name: "Senior Chief",                      file: "RankingIcons/14.png"},
-        {name: "Master Chief",                      file: "RankingIcons/15.png"},
-        {name: "3rd Ensign",                        file: "RankingIcons/16.png"},
-        {name: "2nd Ensign",                        file: "RankingIcons/17.png"},
-        {name: "1st Ensign",                        file: "RankingIcons/18.png"},
-        {name: "Lieutenant Junior Grade 2nd Class", file: "RankingIcons/19.png"},
-        {name: "Lieutenant Junior Grade 1st Class", file: "RankingIcons/20.png"},
-        {name: "3rd Class Lieutenant",              file: "RankingIcons/21.png"},
-        {name: "2nd Class Lieutenant",              file: "RankingIcons/22.png"},
-        {name: "1st Class Lieutenant",              file: "RankingIcons/23.png"},
-        {name: "Lieutenant Commander",              file: "RankingIcons/24.png"},
-        {name: "Commander",                         file: "RankingIcons/25.png"},
-        {name: "Captain",                           file: "RankingIcons/26.png"},
-        {name: "Rear Admiral",                      file: "RankingIcons/27.png"},
-        {name: "Vice Admiral",                      file: "RankingIcons/28.png"},
-        {name: "Admiral",                           file: "RankingIcons/29.png"},
-        {name: "Delegate",                          file: "RankingIcons/30.png"},
-        {name: "President",                         file: "RankingIcons/31.png"}
+        {name: "Citizen",                 file: "RankingIcons/1.png"},
+        {name: "Recruit",                 file: "RankingIcons/2.png"},
+        {name: "4th Class Rookie",        file: "RankingIcons/3.png"}, // 4th Class
+        {name: "2nd Class Rookie",        file: "RankingIcons/4.png"},
+        {name: "2nd Class Rookie",        file: "RankingIcons/5.png"},
+        {name: "1st Class Rookie",        file: "RankingIcons/6.png"},
+        {name: "3rd Class Squire",        file: "RankingIcons/7.png"}, // Spaceship
+        {name: "2nd Class Squire",        file: "RankingIcons/8.png"}, // Spaceship
+        {name: "1st Class Squire",        file: "RankingIcons/9.png"}, // Spaceship
+        {name: "3rd Class Knight",        file: "RankingIcons/10.png"}, // Hexagon
+        {name: "2nd Class Knight",        file: "RankingIcons/11.png"}, // Hexagon
+        {name: "1st Class Knight",        file: "RankingIcons/12.png"}, // Hexagonn
+        {name: "Knight Commander",        file: "RankingIcons/13.png"}, // Chief
+        {name: "Senior Knight Commander", file: "RankingIcons/14.png"}, // Senior Chief
+        {name: "Master Knight Commander", file: "RankingIcons/15.png"}, // Master Chief
+        {name: "3rd Field Officer",       file: "RankingIcons/16.png"}, // Ensign
+        {name: "2nd Field Officer",       file: "RankingIcons/17.png"}, // Ensign
+        {name: "1st Field Officer",       file: "RankingIcons/18.png"}, // Ensign
+        {name: "2nd Class Junior Lt.",    file: "RankingIcons/19.png"},
+        {name: "1st Class Junior Lt.",    file: "RankingIcons/20.png"},
+        {name: "3rd Class Lt.",           file: "RankingIcons/21.png"},
+        {name: "2nd Class Lt.",           file: "RankingIcons/22.png"},
+        {name: "1st Class Lt.",           file: "RankingIcons/23.png"},
+        {name: "Lt. Commander",           file: "RankingIcons/24.png"},
+        {name: "Senior Lt. Commander",    file: "RankingIcons/25.png"},
+        {name: "Captain",                 file: "RankingIcons/26.png"},
+        {name: "Rear Admiral",            file: "RankingIcons/27.png"},
+        {name: "Vice Admiral",            file: "RankingIcons/28.png"},
+        {name: "Admiral",                 file: "RankingIcons/29.png"},
+        {name: "Counselor to President",  file: "RankingIcons/30.png"},
+        {name: "President",               file: "RankingIcons/31.png"}
     ]
 };
 /* beautify preserve:end */
